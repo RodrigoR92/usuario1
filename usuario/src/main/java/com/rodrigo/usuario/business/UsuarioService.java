c8f2c9c4e38ef907ce2c5c88284f6f9b71c4ff5d
Rodrigo <rjuliorigo@gmail.com>
2026-07-22 16:08:24 -0300

    adicionado métodos de gravar endereços e telefone do usuário

--- usuario/src/main/java/com/rodrigo/usuario/business/UsuarioService.java
+++ usuario/src/main/java/com/rodrigo/usuario/business/UsuarioService.java
@@ -78,7 +78,6 @@ public class UsuarioService {
                 new ResourceNotFoundException("id não encontrado " + idEndereco));
 
         Endereco endereco = usuarioConverter.updateEndereco(enderecoDTO, entity);
-
         return usuarioConverter.paraEnderecoDTO(enderecoRepository.save(endereco));
     }
 
@@ -87,7 +86,24 @@ public class UsuarioService {
                 new ResourceNotFoundException("id não encontrado " + idTelefone));
 
         Telefone telefone = usuarioConverter.updateTelefone(dto, entity);
+        return usuarioConverter.paraTelefoneDTO(telefoneRepository.save(telefone));
+    }
 
+    public EnderecoDTO cadastraEndereco(String token, EnderecoDTO dto) {
+        String email = jwtUtil.extrairEmailToken(token.substring(7));
+        Usuario usuario = usuarioRepository.findByEmail(email).orElseThrow(
+                () -> new ResourceNotFoundException("Email não encontrado " + email));
+
+        Endereco endereco = usuarioConverter.paraEnderecoEntity(dto, usuario.getId());
+        return usuarioConverter.paraEnderecoDTO(enderecoRepository.save(endereco));
+    }
+
+    public TelefoneDTO cadastraTelefone(String token, TelefoneDTO dto) {
+        String email = jwtUtil.extrairEmailToken(token.substring(7));
+        Usuario usuario = usuarioRepository.findByEmail(email).orElseThrow(
+                () -> new ResourceNotFoundException("Email não encontrado " + email));
+
+        Telefone telefone = usuarioConverter.paraTelefoneEntity(dto, usuario.getId());
         return usuarioConverter.paraTelefoneDTO(telefoneRepository.save(telefone));
     }
 
